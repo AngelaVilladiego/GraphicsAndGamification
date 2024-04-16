0debe1c2e7c5e63394317ae0a5fa8b1140c5c311
Angela Villadiego <villadia@sheridancollege.ca>
2024-04-15 20:29:16 -0400

    Example 2: Directional Lighting

--- InitOpenGL/OpenGL/Mesh.cpp
+++ InitOpenGL/OpenGL/Mesh.cpp
@@ -134,7 +134,7 @@ void Mesh::SetShaderVariables(glm::mat4 _pv)
 	m_shader->SetVec3("CameraPosition", m_cameraPosition);
 
 	// Configure light
-	m_shader->SetVec3("light.position", m_lightPosition);
+	m_shader->SetVec3("light.direction", -m_lightPosition);
 	m_shader->SetVec3("light.color", m_lightColor); //Light color
 	m_shader->SetVec3("light.ambientColor", { 0.1f, 0.1f, 0.1f }); //set the ambient lighting
 	m_shader->SetVec3("light.diffuseColor", { 1.0f, 1.0f, 1.0f }); //set the diffuse color to white
